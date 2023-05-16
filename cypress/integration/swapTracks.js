2a413e0680ec1d2d4a8a5443c3625cd05d1cdfe5
binalee21 <binalee.kunjadiya@techolution.com>
2023-05-17 00:12:02 +0530

    added custom commands & more assertions

--- cypress/integration/swapTracks.js
+++ cypress/integration/swapTracks.js
@@ -2,28 +2,21 @@ import constant from "../fixtures/example.json";
 
 describe('swap creation and deletion', () => {
     
-    it('create a swap, verify and delete the swap created', () => {
+    it.only('create a swap, verify and delete the swap created', () => {
         //Navigate to swaps dashboard and swap popup
+        cy.get('.app-name').should('have.text','Playlists');
         cy.get('a[routerlink="/swaps"]').click();
+        cy.get('button[nztype="primary"]').should('have.text','New Swap');
         cy.get('button[nztype="primary"]').click();
 
-        //create new swap and add swap details
-        cy.get('[formcontrolname="searchBy"]').first().click();
-        cy.get('[title="Search By ID"] div').click();
-        cy.get('.search').first().type(constant.firstTrack);
-        cy.get('.ant-list-item-meta-title').click();
-        cy.get('.search').last().type(constant.secondTrack);
-        cy.get('.ant-list-item-meta-title').first().click();
-
-        //submit swap popup and verify success message
-        cy.get('[nztype="primary"]').contains(' Submit ').click();
-        cy.get('.ant-notification-notice-message').should('be.visible');
+        //calling custom command to create swap
+        cy.createSwapAndSubmit(constant.firstTrack, constant.secondTrack);
 
         //verify created swap track details in swap dashboard
         cy.get('tr:nth-child(1) td:nth-child(1)').should('have.text', constant.firstTrack);
         cy.get('tr:nth-child(1) td:nth-child(6)').should('have.text', constant.secondTrack);
 
-        //verify upload button tooltip text of first swap
+        //verify upload button tooltip text of first swap with mouseEnter event
         cy.get('[nztype="upload"]')
             .eq(0)
             .invoke('show')
@@ -31,10 +24,12 @@ describe('swap creation and deletion', () => {
             .wait(1000);        
         cy.get('.ant-tooltip-inner')
             .should('have.text', 'Modify and publish all dynamic playlists containing this track');
+
+        //calling custom command to delete swap
+        cy.deleteSwap(0);  
         
-        //delete created swap and verify deletion message
-        cy.get('button[nztype="delete"]').first().click();
-        cy.get('.ant-btn').contains(' OK ').click();
-        cy.get('.ant-notification-notice-description').should('have.text', 'Swap deleted successfully.');
+        //verify swap dashboard for recently deleted swap
+        cy.get('a[routerlink="/swaps"]').click();
+        cy.get('tr:nth-child(1) td:nth-child(1)').should('not.have.text', constant.firstTrack);
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,34 @@ Cypress.Commands.add("login", (refreshToken) => {
       cy.visit("/");
     });  
   });
-  
+
+Cypress.Commands.add("createSwapAndSubmit", (firstTrack, secondTrack) => {
+  //create new swap and add swap details
+  cy.get('.ant-modal-title').should('have.text','Create Swap');        
+  cy.get('[formcontrolname="searchBy"]').first().click();
+  cy.get('[title="Search By ID"] div').click();
+  cy.get('[formcontrolname="searchBy"]').first().should('have.text','Search By ID');  
+  cy.get('.search').first().type(firstTrack);
+  cy.get('.ant-list-item-meta-title').click();
+  cy.get('.display').first().should('have.text',firstTrack);
+  cy.get('.search').last().type(secondTrack);
+  cy.get('.ant-list-item-meta-title').first().click();
+  cy.get('.display').last().should('not.have.text',secondTrack);
+
+  //submit swap popup and verify success message
+  cy.get('[nztype="primary"]').contains(' Submit ').click();
+  cy.get('.ant-notification-notice-message').should('be.visible');
+});
+
+Cypress.Commands.add("deleteSwap", (index) => {
+  cy.get('button[nztype="delete"]').eq(index).click();
+  cy.get('.ant-modal-confirm-title').should('have.text', 'Delete Swap?');        
+  cy.get('.ant-btn').contains(' OK ').click();
+  cy.wait(1000);
+  cy.get('.ant-notification-notice-description')
+    .contains('Swap deleted successfully.')
+    .should('be.visible');
+});
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
