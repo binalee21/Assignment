2bbab84397bfa83faa48198d4b97a0dd5e1751cb
binalee21 <binalee.kunjadiya@techolution.com>
2023-05-17 12:48:00 +0530

    added custom commads

--- cypress/integration/moveTrackOrder.js
+++ cypress/integration/moveTrackOrder.js
@@ -1,43 +1,38 @@
-import constant from "../fixtures/example.json";
-
 describe('Move track order and verify', () => {
     let trackTitle = "";
+    const startRow = 1;
+    const destinationRow = 10;
+    let flag = false;
 
-    it('search test playlist, fetch track title text which need to be move from playlist', () => {
-        //searching for specific testplaylist 
-        cy.get('.app-name').should('have.text', 'Playlists');
-        cy.get('input[placeholder="Search Title"]').should('be.visible');
-        cy.get('[placeholder="Search Title"]').type(constant.testPlaylistName);
-        cy.wait(4000);
-
+    it('Navigate to first playlist, fetch track title text which need to be move from playlist', () => {
         //Navigating to first test Playlist page and verify title
-        cy.get('.clickable').contains(constant.testPlaylistName).should('be.visible');
+        cy.get('.app-name').should('have.text','Playlists');
         cy.get('.clickable').eq(0).click();
-        cy.get('.ant-card-meta-title').should('have.text', constant.testPlaylistName);
-
-        //fetch any track title
-        cy.get('.tracks-row:nth-child(4) div:nth-child(4)')
-            .invoke("text")
-            .then( text => {
-                trackTitle = text;
-            });
+        cy.contains(' Edit Playlist ').should('be.visible');
+       
+        //fetch track title of track which needs to be move
+        cy.get('.tracks-row').then( $el => {
+            if($el.length >= startRow && $el.length >= destinationRow) {
+                flag = true;
+                cy.get(`.tracks-row:nth-child(${startRow+1}) div:nth-child(4)`)
+                .invoke("text")
+                .then( text => {
+                    trackTitle = text;
+                });
+            }
+        });
+        
     });
 
-    it('move the track to down, verify new order', () => {        
-        //move track title to down
-        cy.get('.tracks-row:nth-child(4) [nztype="arrow-down"]').click();
-
-        //verify new order of moved track title
-        cy.get('.tracks-row:nth-child(4) div:nth-child(4)').should('not.have.text', trackTitle);
-        cy.get('.tracks-row:nth-child(5) div:nth-child(4)').should('have.text', trackTitle);
+    it('move the track to down, verify new order', () => {
+        //fetch total track count, compare length with start and destination row        
+        if(flag == true) {
+            //custom command to move tracks down
+            cy.moveTrackDown(startRow, destinationRow, trackTitle);
+            //custom command to move tracks up
+            cy.moveTrackUp(startRow, destinationRow, trackTitle);
+        } else {
+            cy.log("**** There are not enough Tracks to move! ****");
+        }                      
     });
-
-    it('move the track to up, verify new order', () => { 
-        //move track title to up
-        cy.get('.tracks-row:nth-child(5) [data-icon="arrow-up"]').click();
-
-        //verify new order of moved track title
-        cy.get('.tracks-row:nth-child(4) div:nth-child(4)').should('have.text', trackTitle);
-        cy.get('.tracks-row:nth-child(5) div:nth-child(4)').should('not.have.text', trackTitle);
-    });
-});
+ });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,34 @@
 
 Cypress.Commands.add("login", (refreshToken) => {
-    cy.request({
-      method: "POST",
-      url: `https://auth.radioedit.ihrint.com/token?grant_type=refresh_token&refresh_token=${refreshToken}`
-    }).then(response => {
-      const accessToken = response.body.access_token;
-      cy.setCookie('access_token', accessToken);
-      cy.setCookie('access_token_secure', accessToken);
-      cy.visit("/");
-    });  
-  });
+  cy.request({
+    method: "POST",
+    url: `https://auth.radioedit.ihrint.com/token?grant_type=refresh_token&refresh_token=${refreshToken}`
+  }).then(response => {
+    const accessToken = response.body.access_token;
+    cy.setCookie('access_token', accessToken);
+    cy.setCookie('access_token_secure', accessToken);
+    cy.visit("/");
+  });  
+});
   
+Cypress.Commands.add("moveTrackDown", (startRow, destinationRow, trackTitle) => {
+  const numberOfMoves = destinationRow - startRow;
+  cy.log(numberOfMoves);
+  for(let index = 1; index<=numberOfMoves; index++) {
+    cy.get(`.tracks-row:nth-child(${startRow + index}) [nztype="arrow-down"]`).click();
+  }
+  cy.get(`.tracks-row:nth-child(${destinationRow+1}) div:nth-child(4)`).should('have.text', trackTitle);
+});
+
+Cypress.Commands.add("moveTrackUp", (startRow, destinationRow, trackTitle) => {
+  const numberOfMoves = Math.abs(startRow - destinationRow);
+  cy.log(numberOfMoves);
+  for(let index = 0; index<numberOfMoves; index++) {
+    cy.get(`.tracks-row:nth-child(${destinationRow + 1 - index}) [nztype="arrow-up"]`).click();
+  }
+  cy.get(`.tracks-row:nth-child(${startRow+1}) div:nth-child(4)`).should('have.text', trackTitle);
+});
+
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
