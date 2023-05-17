fcce1f1dc090ba8f5fb17eaf2560a9e76da4e02b
binalee21 <binalee.kunjadiya@techolution.com>
2023-05-18 02:56:54 +0530

    added more assertions

--- cypress/integration/assignment2.js
+++ cypress/integration/assignment2.js
@@ -1,19 +1,41 @@
 describe('Display track number from Dynamic playlist', () => {
+    let trackTitle = "";
 
-    it('Select a Dynamic playlist and fetch the track number and display it', () => {
+    it('Select a Dynamic playlist and fetch the track title', () => {
         //select dynamic playlist option from filter
+        cy.get('.app-name').should('have.text','Playlists');
+        cy.get('button[data-name="playlist-list-filter"]').should('be.visible');        
         cy.get('button[data-name="playlist-list-filter"]').click();
+        cy.get('label').contains('Type').should('be.visible');
         cy.get('nz-select[formcontrolname="playlistType"]').click();
         cy.get('nz-option-item[title="Dynamic"] div').click();
+        cy.get('[title="Dynamic"]').should('be.visible');
         cy.wait(2000);
 
         //Navigate to first filtered playlist
         cy.get('.clickable').eq(0).click();
+        cy.get('[nztype="warning"].ant-typography')
+            .should('have.text', 'This playlist is dynamically updated. Any changes made here will be overridden when new playlists are published by Data Science.  ');
+            
+        //fetch first track title
+        cy.get('playlist-tracks').first().find(`.tracks-row:nth-child(2) div:nth-child(4)`)
+            .invoke("text")
+            .then( text => {
+                trackTitle = text;
+            }); 
+    });
 
+    it('open track detail, fetch track number and display it', () => {
         //click to open track details
-        cy.get('[nztype="info-circle"]').eq(0).click();
+        cy.get('playlist-tracks')
+            .first()
+            .find('.tracks-row:nth-child(2)')
+            .find('[nztype="info-circle"]')
+            .click();
 
-        //fetch track number and display it
+        //verify detail popup, fetch track number and display it
+        cy.get('.bold a').should('have.text', trackTitle.trim());
+        cy.get('.ant-popover-inner-content').contains('Track ID').should('be.visible');
         cy.get('.ant-popover-inner-content').contains('Track ID')
             .then(($el) => {
             const trackNumber = $el.text().split('Track ID:');
